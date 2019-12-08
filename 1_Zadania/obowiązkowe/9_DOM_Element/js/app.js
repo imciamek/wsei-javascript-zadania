0e46f395f718741fd8477c4ff11eba83b33d0af1
Oleksy Maciej <maciej.oleksy@microsoft.wsei.edu.pl>
2019-12-08 15:30:38 +0100

    Task 9

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -5,8 +5,71 @@ document.addEventListener("DOMContentLoaded", function () {
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
 
-    /*
-    Poniżej napisz kod rozwiązujący zadania
-     */
 
-});
+//Zadanie 0
+function getDatasInfo(elements)
+{
+    var array = [];
+    for(var i = 0; i < elements.length; i++) {
+        array.push(elements[i].dataset);
+    }
+    return array;
+}
+
+console.log(getDatasInfo(links));
+
+
+//Zadanie 1
+console.log(homeElement);
+console.log(banner);
+
+for(let i = 0; i < childElements.length; i++) {console.log(childElements[i]);}
+
+for(let i = 0; i < blocks.length; i++) {console.log(blocks[i].innerHTML);}
+
+for(let i = 0; i < links.length; i++) {console.log(links[i]);}
+
+//Zadanie 2
+for(let item of blocks){
+    console.log(item.innerHTML);
+    console.log(item.outerHTML);
+   }
+   blocks.innerHtml = 'Nowa wartość diva o klasie blocks';
+
+    // innerHTML wyświetla strukturę dom wewnątrz elementu
+    // outerHTML wyświetla wraz z elementem
+
+//Zadanie 3
+var mainFooter = document.querySelector('#mainFooter');
+console.log(mainFooter);
+
+function getId(element) {
+    const id = element.getAttribute('id');
+    console.log(id);
+    return id;
+}
+
+//Zadanie 4
+function getTags(elements){
+    var tags = [];
+   for(let item of elements){
+        tags.push(item.tagName);
+   }
+   return tags;
+}
+//Zadanie 5
+function getClassInfo(element){
+     return element.classList;
+    }
+    getClassInfo(banner);
+
+//Zadanie 6
+const liElements = document.querySelectorAll('nav li');
+function setDataDirection(elements){
+    for(let item of elements){
+        if(!item.getAttribute('data-direction')){
+             item.setAttribute('data-direction','top');
+        }
+    }
+}
+}); 
